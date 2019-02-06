9f472c914e50a92a19d8c7a588aa1c3148c5f39d
shibhash <sahooshib1@gmail.com>
2019-02-06 18:52:54 +0530

    Redundant npm packages removed

--- src/Components/HeaderBlock.js
+++ src/Components/HeaderBlock.js
@@ -5,7 +5,6 @@ function HeaderBlock(props) {
             <div className = {props.className}>
                 <h2>{props.title}</h2>
                 <p>{props.description}</p>
-
                 <br />
             </div>
         );
